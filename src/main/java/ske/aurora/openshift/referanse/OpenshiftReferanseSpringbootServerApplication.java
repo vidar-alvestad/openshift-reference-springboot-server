84bb6ee64c7c3461ff4a6b813372b8a9d12eaf29
Bent André Solheim <BentAndre.Solheim@skatteetaten.no>
2016-09-14 12:23:41 +0200

    Initial import from using the Spring Initializer with maven and web dependency

---
+++ src/main/java/ske/aurora/openshift/referanse/OpenshiftReferanseSpringbootServerApplication.java
@@ -0,0 +1,12 @@
+package ske.aurora.openshift.referanse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftReferanseSpringbootServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftReferanseSpringbootServerApplication.class, args);
+	}
+}
