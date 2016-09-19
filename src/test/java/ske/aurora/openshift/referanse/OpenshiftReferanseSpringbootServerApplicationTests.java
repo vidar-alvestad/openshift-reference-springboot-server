44cf245ef1f0c2876c67afaccc804d730cd08a62
Are Vattekar <are.vattekar@skatteetaten.no>
2016-09-19 12:00:12 +0200

    AOS-452 - Added sample test in order to pass mutation testing and various code examples

--- src/test/java/ske/aurora/openshift/referanse/OpenshiftReferanseSpringbootServerApplicationTests.java
+++ src/test/java/ske/aurora/openshift/referanse/OpenshiftReferanseSpringbootServerApplicationTests.java
@@ -1,13 +1,41 @@
 package ske.aurora.openshift.referanse;
 
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import ske.aurora.openshift.referanse.test.ApplicationConstants;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OpenshiftReferanseSpringbootServerApplicationTests {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+
+    }
+
+    @Test
+    public void liveliness() throws Exception {
+        mockMvc.perform(get(ApplicationConstants.PING_URL))
+            .andExpect(status().isOk());
+    }
 
 	@Test
 	public void contextLoads() {
