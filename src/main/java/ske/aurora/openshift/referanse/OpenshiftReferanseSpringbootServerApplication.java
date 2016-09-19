39b96ed8043073f523d0a474028f028056ae2de3
Are Vattekar <are.vattekar@skatteetaten.no>
2016-09-19 13:16:42 +0200

    AOS-452: Corrected Checkstyle errors

--- src/main/java/ske/aurora/openshift/referanse/OpenshiftReferanseSpringbootServerApplication.java
+++ src/main/java/ske/aurora/openshift/referanse/OpenshiftReferanseSpringbootServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OpenshiftReferanseSpringbootServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OpenshiftReferanseSpringbootServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OpenshiftReferanseSpringbootServerApplication.class, args);
+    }
 }
