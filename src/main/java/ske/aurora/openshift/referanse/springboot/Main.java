942b49e179f1e7e25199d9057d6f08a6c936ab73
Bent André Solheim <BentAndre.Solheim@skatteetaten.no>
2016-09-20 14:02:24 +0200

    Sample endpoints for testing metrics

--- src/main/java/ske/aurora/openshift/referanse/springboot/Main.java
+++ src/main/java/ske/aurora/openshift/referanse/springboot/Main.java
@@ -2,11 +2,25 @@ package ske.aurora.openshift.referanse.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @RequestMapping("/test")
+    public String test() {
+        return "test";
+    }
+
+
+    @RequestMapping("/api/test")
+    public String apiTest() {
+        return "apiTest";
     }
 }
