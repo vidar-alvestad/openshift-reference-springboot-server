46e74e986d232bc91951d83a8d249d46e9dd8e04
Bent André Solheim <BentAndre.Solheim@skatteetaten.no>
2016-09-21 13:07:41 +0200

    Fixed issue with service name becoming too long

--- openshift/setup_dev.sh
+++ openshift/setup_dev.sh
@@ -6,13 +6,13 @@ oc new-project mfp-openshift-referanse-springboot-server
 sleep 1
 
 oc new-app --template=aurora-development-3.0 \
-    -p APP_NAME=openshift-referanse-springboot-server \
+    -p APP_NAME=springboot-server \
     -p ARTIFACT_ID=openshift-referanse-springboot-server \
     -p GROUP_ID=ske.aurora.openshift.referanse \
     -p VERSION=0.0.1-SNAPSHOT \
     -p MAX_MEMORY=512M \
-    -l app=openshift-referanse-springboot-server
+    -l app=springboot-server
 
-oc start-build openshift-referanse-springboot-server
+oc start-build springboot-server
 
 oc policy add-role-to-user edit system:serviceaccount:mfp-jenkins:jenkins-builder -n mfp-openshift-referanse-springboot-server
