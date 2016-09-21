eadb7e195e03a34d009f50e70724aa0bf2480f95
Bent André Solheim <BentAndre.Solheim@skatteetaten.no>
2016-09-21 21:28:09 +0200

    Refactor

--- src/main/java/ske/aurora/openshift/referanse/springboot/Main.java
+++ src/main/java/ske/aurora/openshift/referanse/springboot/Main.java
@@ -1,6 +1,7 @@
 package ske.aurora.openshift.referanse.springboot;
 
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Properties;
 
 import org.springframework.boot.SpringApplication;
@@ -16,20 +17,33 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        String databasePath = System.getenv("REFERANSEAPP_DB_PROPERTIES");
-
-        if (databasePath != null) {
-            Properties props = new Properties();
-            try (FileReader reader = new FileReader(databasePath)) {
-                props.load(reader);
-            }
-
-            System.setProperty("spring.datasource.url", props.getProperty("jdbc.url"));
-            System.setProperty("spring.datasource.username", props.getProperty("jdbc.user"));
-            System.setProperty("spring.datasource.password", props.getProperty("jdbc.password"));
-        }
+        setDatasourcePropertiesFromDbPropertiesFile("REFERANSEAPP_DB_PROPERTIES");
 
         SpringApplication.run(Main.class, args);
+    }
+
+    /**
+     * Denne metoden leser JDBC tilkoblingsinformasjon fra en properties fil i formatet fra Sprocket og setter
+     * tilsvarende spring relaterte datasource properties.
+     *
+     * @param propertiesFile
+     * @throws IOException
+     */
+    private static void setDatasourcePropertiesFromDbPropertiesFile(String propertiesFile) throws IOException {
+
+        String databasePath = System.getenv(propertiesFile);
+
+        if (databasePath == null) {
+            return;
+        }
+        Properties props = new Properties();
+        try (FileReader reader = new FileReader(databasePath)) {
+            props.load(reader);
+        }
+
+        System.setProperty("spring.datasource.url", props.getProperty("jdbc.url"));
+        System.setProperty("spring.datasource.username", props.getProperty("jdbc.user"));
+        System.setProperty("spring.datasource.password", props.getProperty("jdbc.password"));
     }
 
     @RequestMapping("/test")
