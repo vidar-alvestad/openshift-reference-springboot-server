6a3b7eacead76295e3728597321a55e123039c10
Bent André Solheim <BentAndre.Solheim@skatteetaten.no>
2016-12-13 10:41:14 +0100

    Example counter

--- src/main/java/ske/aurora/openshift/referanse/springboot/controllers/CounterController.java
+++ src/main/java/ske/aurora/openshift/referanse/springboot/controllers/CounterController.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codahale.metrics.annotation.Timed;
+
 import ske.aurora.openshift.referanse.springboot.service.CounterService;
 
 @RestController
@@ -21,6 +23,7 @@ public class CounterController {
         this.counterService = counterService;
     }
 
+    @Timed(name = "counter")
     @GetMapping("/api/counter")
     public Map<String, Object> counter() {
 
