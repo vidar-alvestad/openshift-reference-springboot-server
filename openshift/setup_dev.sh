613cd60576358f355432cd3a94c6f05dca22c8f1
Bent André Solheim <BentAndre.Solheim@skatteetaten.no>
2017-01-20 10:23:59 +0100

    Updated deploy script

--- openshift/setup_dev.sh
+++ openshift/setup_dev.sh
@@ -5,16 +5,12 @@ sleep 5
 oc new-project mfp-openshift-referanse-springboot-server
 sleep 1
 
-oc new-app --template=aurora-development-3.0 \
-    -p APP_NAME=springboot-server \
-    -p ARTIFACT_ID=openshift-referanse-springboot-server \
-    -p GROUP_ID=ske.aurora.openshift.referanse \
-    -p VERSION=0.0.1-SNAPSHOT \
-    -p MAX_MEMORY=512M \
-    -p PROMETHEUS_ENABLED=true \
+oc new-app --template=aurora-deploy-3.0 \
+    -p APP_NAME=demo \
+    -p AFFILIATION=aurora \
+    -p DOCKER_GROUP=ske_aurora_openshift_referanse \
+    -p DOCKER_NAME=openshift-referanse-springboot-server \
+    -p TAG=dev \
     -p DATABASE=referanseapp:3f9d8558-0fd2-488e-8f79-fbbdf62b04ff \
-    -l app=springboot-server
-
-oc start-build springboot-server
-
-oc policy add-role-to-user edit system:serviceaccount:mfp-jenkins:jenkins-builder -n mfp-openshift-referanse-springboot-server
+    -p MAX_MEMORY=512M \
+    -p PROMETHEUS_ENABLED=true
