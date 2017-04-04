d0913d7c5b7a279a7c1faf6422840a48c44315a3
Bjarte Stien Karlsen <Bjarte.Karlsen@skatteetaten.no>
2017-04-04 10:37:16 +0200

    endrer log nivå

--- src/main/java/ske/aurora/openshift/referanse/springboot/Main.java
+++ src/main/java/ske/aurora/openshift/referanse/springboot/Main.java
@@ -20,7 +20,7 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
-        LOG.info("Setter opp database med navn REFERANSEAPP");
+        LOG.debug("Setter opp database med navn REFERANSEAPP");
         setDatasourcePropertiesFromDbPropertiesFile("REFERANSEAPP_DB_PROPERTIES");
 
         SpringApplication.run(Main.class, args);
