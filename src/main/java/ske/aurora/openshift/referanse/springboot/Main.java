5c49aaf7dc07dd9a8fa7b73aa9753d84ddf58a5d
Bjarte Stien Karlsen <Bjarte.Karlsen@skatteetaten.no>
2017-05-05 14:34:41 +0200

    move to starters

--- src/main/java/ske/aurora/openshift/referanse/springboot/Main.java
+++ src/main/java/ske/aurora/openshift/referanse/springboot/Main.java
@@ -2,8 +2,12 @@ package ske.aurora.openshift.referanse.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@Configuration
+@PropertySource("classpath:aurora-openshift-spring-boot-starter.properties")
 public class Main {
 
     protected Main() {
