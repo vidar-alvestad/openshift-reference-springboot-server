497b2cd911f713308e4b2ff64dfb065ddcc78da0
Bjarte Stien Karlsen <Bjarte.Karlsen@skatteetaten.no>
2017-05-07 22:40:42 +0200

    fixed http get request controller to just get ip. Use starters from the AOS-1478 branch

--- src/main/java/ske/aurora/openshift/referanse/springboot/controllers/IpController.java
+++ src/main/java/ske/aurora/openshift/referanse/springboot/controllers/IpController.java
@@ -1,11 +1,10 @@
 package ske.aurora.openshift.referanse.springboot.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 /*
  * An example controller that does a request to another resource
@@ -13,23 +12,19 @@ import org.springframework.web.client.RestTemplate;
  * There should be a metric called http_client_request_bucket recorded with the response
  */
 @RestController
-public class SkatteetatenController {
-
-    private static final Logger logger = LoggerFactory.getLogger(SkatteetatenController.class);
+public class IpController {
 
     private RestTemplate restTemplate;
 
-    public SkatteetatenController(RestTemplate restTemplate) {
+    public IpController(RestTemplate restTemplate) {
 
         this.restTemplate = restTemplate;
     }
 
-    @GetMapping("/api/skatteetaten")
-    public String skatteetaten() {
-
-        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://skatteetaten.no", String.class);
-        logger.info("{}", forEntity.getStatusCode());
-        return forEntity.getBody();
+    @GetMapping("/api/ip")
+    public String ip() {
+        JsonNode forEntity = restTemplate.getForObject("http://httpbin.org/ip", JsonNode.class);
+        return forEntity.get("origin").textValue();
     }
 
 }
